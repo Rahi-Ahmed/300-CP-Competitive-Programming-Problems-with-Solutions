728fbb476d77fc4127f59880a5c5059874ce09f3
Rahi Ahmed <87031439+Rahi-Ahmed@users.noreply.github.com>
2024-07-12 09:27:26 -0400

    Update Max_Row.c

--- Dimik_OJ/Max_Row.c
+++ Dimik_OJ/Max_Row.c
@@ -2,6 +2,7 @@
 
 // Finding Maximum 1's in a row
 // Running in O(n) time for finding the row that contains the most 1's
+// Assuming all the 1's come before 0's in the row
 
 int main(){
     int a[3][3] = {{1, 0, 1}, {1, 1, 1}, {0, 0, 0}};
@@ -21,3 +22,29 @@ int main(){
 
     printf("Max number of 1's is in row %d \n", max_row);
 }
+
+
+// Using for loop
+/*#include <stdio.h>
+
+int main() {
+    int a[3][3] = {{0, 1, 1}, {0, 1, 1}, {1, 1, 1}};
+    int column_size = 3;
+    int row_size = 3;
+
+    int max_row = 0;
+    int column = -1, row = 0;
+    for(; row < row_size && column < column_size; ){
+        if(a[row][column+1] == 1){
+            max_row = row;
+            column++;
+        }else{
+            row++;
+        }
+    }
+
+    printf("Max number of 1's is in row %d \n", max_row);
+
+    return 0;
+}*/
+
