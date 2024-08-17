a86bff4d75264b686de97e368d61436d4b32329a
Rahi Ahmed <87031439+Rahi-Ahmed@users.noreply.github.com>
2024-08-17 09:34:27 -0400

    Add files via upload

---
+++ Phase_01/Forgotten_Episode.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    bool watched[n + 1];
+
+    for(int i =1; i <= n; i++){
+        watched[i] = false;
+    }
+
+    for(int i = 1; i < n; i++){
+        int episode;
+        cin >> episode;
+        watched[episode] = true;
+    }
+
+
+    int not_watched = -1;
+    for(int i = 1; i <= n; i++){
+        if(!watched[i]){
+           not_watched = i;
+        }
+    }
+
+    cout << not_watched << endl;
+    return 0;
+}
